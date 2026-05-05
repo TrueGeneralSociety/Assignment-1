3df69564d2bff583b3e4ba3d404d6aa4d084bf67
Samien Munwar <munwarsamien@gmail.com>
2026-05-04 17:40:30 -0700

    build folder added

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ connectDB().catch((err) => {
   process.exit(1);
 });
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
